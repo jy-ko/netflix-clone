e0fe8835ea10e6a1ec06c28f61dba0face6626f5
Jiyoung Ko <jiyoungko@192.168.68.104>
2022-09-03 22:49:57 +0300

    circle animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './routes/Home';
-import Movies from './routes/Movies';
-import Search from './routes/Search';
-import { GlobalStyles } from './components/globalStyles';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+import Movies from "./routes/Movies";
+import Search from "./routes/Search";
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      <Navbar/>
-      < BrowserRouter>
-          <Routes>
-            <Route path="/" element={< Home />}></Route>
-            <Route path="/movies" element={< Movies />}></Route>
-            < Route path="/search" element={< Search />}></Route>
-          </Routes>
-        </BrowserRouter>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
